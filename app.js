10c91f3939e6c4c1d3b51b1b5ac2d3dd9c122786
danielurban1 <urbandaniel289@gmail.com>
2017-07-12 11:27:30 +0200

    updated

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {Userslist} = require('./models/user');
+// var {mongoose} = require('./db/mongoose');
+// var {Todo} = require('./models/todo');
+// var {Userslist} = require('./models/user');
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todo', (req, res) =>{
-  console.log("body", req.body);
-  var todo = new Todo({
-    text: req.body.text
-  });
-  todo.save().then((doc) =>{
-    console.log('Saved', req.body.text);
-    res.status(400).send(doc);
-  }, (err) =>{
-
-    console.log(err);
-  })
+app.get('/asad', (req, res) =>{
+  res.send('ok');
 });
+
+// app.post('/todo', (req, res) =>{
+//   console.log("body", req.body);
+//   var todo = new Todo({
+//     text: req.body.text
+//   });
+//   todo.save().then((doc) =>{
+//     console.log('Saved', req.body.text);
+//     res.status(400).send(doc);
+//   }, (err) =>{
+
+//     console.log(err);
+//   })
+// });
 
 app.listen(3000, ()=> {
   console.log('Started at', mongoose.connections);
--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var uristring =
     process.env.MONGOLAB_URI ||
-    process.env.MONGOHQ_URL || 'mongodb://localhost:27017/App';
+    process.env.MONGOHQ_URL || 'mongodb://localhost:27017/';
 
 mongoose.Promise = global.Promise;
 
