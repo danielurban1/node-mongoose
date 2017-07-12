a73a642497d0853be0da0a260a7c75414a84f55a
danielurban1 <urbandaniel289@gmail.com>
2017-07-12 15:44:40 +0200

    Updated

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 
 var uristring =
-    process.env.MONGOLAB_URI ||
-    process.env.MONGOHQ_URL || 'mongodb://localhost:27017/App';
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/App';
 
 mongoose.Promise = global.Promise;
 
